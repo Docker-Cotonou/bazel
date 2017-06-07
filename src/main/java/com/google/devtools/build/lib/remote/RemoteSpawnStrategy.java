6ac078873731d8f25d6fbacfdd667e2855653b1c
Mike Morearty <mike@asana.com>
2017-06-06 17:17:01 -0700

    wip

--- src/main/java/com/google/devtools/build/lib/remote/RemoteSpawnStrategy.java
+++ src/main/java/com/google/devtools/build/lib/remote/RemoteSpawnStrategy.java
@@ -297,6 +297,7 @@ final class RemoteSpawnStrategy implements SpawnActionContext {
               actionExecutionContext.getActionInputFileCache(),
               actionExecutionContext.getExecutor().getContext(FilesetActionContext.class));
       TreeNode inputRoot = repository.buildFromActionInputs(inputMap);
+      System.err.println(">>>RemoteSpawnStrategy " + spawn.getMnemonic() + " " + spawn.getOutputFiles()[0] + ":\n" + inputRoot.toDebugString());
       repository.computeMerkleDigests(inputRoot);
       Command command = buildCommand(spawn.getArguments(), spawn.getEnvironment());
       Action action =
